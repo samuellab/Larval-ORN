4e7240665730b3cd651331bc2c97a7b77c016691
Guangwei <siguangwei@gmail.com>
2019-01-11 15:20:46 -0500

    uploaded datas1

---
+++ Figure2/GetAveData.m
@@ -0,0 +1,59 @@
+function [ dataMean, dataSEM, odorList, concList, ORNList ] = GetAveData( )
+%% Get the averaged matrix from the raw data
+
+% read raw data
+[ dataRaw, infoOdorRaw, ~, infoConcRaw, ORNList] = ReadRawData();
+% [~, dataRaw, infoOdorRaw, ~, infoConcRaw, ORNList] = ReadRawData();
+
+[odorList, ~] = unique(infoOdorRaw, 'stable');  %list of test odors
+[concList_full, ~] = unique(infoConcRaw);            %list of test concentrations
+
+% correct typos of odor name
+odorList{find(strcmp(odorList,'4-methylcyclohexane' ))} = '4-methylcyclohexanol ';
+odorList{find(strcmp(odorList,'4-pheny-2-butanol' ))} = '4-phenyl-2-butanol';
+
+%
+if length(concList_full) > 5
+    concList = concList_full(end-4 : end);
+else
+    concList = concList_full;
+end
+
+%% mean and SEM of the data
+dataMean = zeros(length(odorList), length(ORNList), length(concList));
+dataSEM  = zeros(length(odorList), length(ORNList), length(concList)); %standard error of the mean
+
+for i = 1: length(odorList) %go through odors
+   od = odorList{i};
+   indexTemp = strfind(infoOdorRaw, od);
+   index_odor = find(~cellfun('isempty', indexTemp));
+   
+   % go through concentrations
+   for j = 1 : length(concList)
+       index_con = find(infoConcRaw(index_odor) == concList(j));
+       rowList = index_odor(index_con);
+       dataBlock = dataRaw(rowList, :);
+       for k = 1:length(ORNList) %go though ORNs
+           dataVec = dataBlock(:, k);
+           index_nnan = find(~isnan(dataVec));
+           dataVecPure = dataVec(index_nnan);
+           dataMean(i, k, j) = mean(dataVecPure);
+           dataSEM(i, k, j)  = std(dataVecPure)/sqrt(length(dataVecPure));
+       end
+   end
+end
+
+% %% visualize the data, roughly
+% for i = 1:length(concList)
+%     dataMat = dataMean(:, :, i);
+%     figure; imagesc(dataMat); title(['mean ', num2str(concList(i))]);
+%     
+%     semMat = dataSEM(:,:,i);
+%     figure; imagesc(semMat); title(['SEM ', num2str(concList(i))]);
+% end
+
+%% save data
+dataSaveFile = fullfile('..', 'data', 'AveRawDataMatrix2ndRound.mat'); 
+save(dataSaveFile, 'dataMean', 'dataSEM', 'odorList', 'concList', 'ORNList');
+
+end
---
+++ Figure2/ReadRawData.m
@@ -0,0 +1,48 @@
+function [ dataRaw, infoOdorRaw, infoExpIDRaw, infoConcRaw, infoORNList] = ReadRawData()
+%% Read the .xlsx data file in the subfolder of 'data',
+% no input varialbes,
+% outputs are the raw data matrix and information of the experiments
+
+filename = fullfile('..', 'data_2nd_round', 'Supplementary Table 1_plusNewData.xlsx'); %define the .xlsx file
+sheet = 1;
+xlRange = 'A1:X1196';
+[~,~,raw] = xlsread(filename,sheet,xlRange);
+
+%define the experiment information list
+infoOdorRaw  = raw(2:end, 1);
+infoExpIDRaw = raw(2:end, 2);
+infoConcRaw  = cell2mat(raw(2:end, 3));
+infoORNList  = raw(1, 4:end);
+
+%%
+%extract the data matrix
+A = importdata(filename);
+B=A.data.Sheet1;
+dataRaw = B (:, 3:end);
+
+%% clean the data
+% % find out the unidentified (no activity) neurons
+% index0 = sum(dataRaw, 1)==0;
+% disp('List of unidentified neurons:');
+% disp(infoORNList(index0));
+
+% remove the unidentified neurons in the data and ORN list, and rewrite the varaibles
+% index1 = sum(dataRaw, 1)~=0;
+% infoORNList = infoORNList(index1);
+% dataRaw = dataRaw(:, index1);
+
+% %% organize the informaiton and data into a table
+% tColHeader = [{'Odor', 'Exp_ID', 'Concentration'}, infoORNList];
+% tData = cell(length(dataRaw), length(tColHeader));
+% tData(:, 1)     = infoOdorRaw;
+% tData(:, 2)     = infoExpIDRaw;
+% tData(:, 3)     = num2cell(infoConcRaw);
+% tData(:, 4:end) = num2cell(dataRaw);
+% 
+% rawT = cell2table( tData , 'VariableNames',tColHeader); %define the table
+% 
+% %%
+% infoORNList{1}  = 'Or33b-47a';
+% infoORNList{18} = 'Or94a-94b';
+
+end
